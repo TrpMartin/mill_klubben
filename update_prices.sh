70ef5a527bed385e1fb8c8ea702ce5b4cd320afd
TrpMartin <47293905+TrpMartin@users.noreply.github.com>
2026-04-07 20:25:36 +0200

    Delete update_prices.sh

--- update_prices.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-
-MKPATH=/home/pi/mynotebooks/mill_klubben
-echo "Changing to {$MKPATH}"
-cd $MKPATH
-echo "Download starting from virtual env..."
-/home/pi/jupytervenv/bin/python3 dl_mill_klubben_prices.py
